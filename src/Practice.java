db2fc52dde4411aa84994f0d211fe8ea1d26944f
JaewonJeung <49247410+JaewonJeung@users.noreply.github.com>
2020-05-16 00:01:08 -0400

    Deleted a portion of print (#5)
    * Deleted a portion of print

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,6 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("make sure you pull and resolve potential conflicts before you commit and push!");
+        System.out.println("make sure you pull good change here");
         //nice
     }
 }
