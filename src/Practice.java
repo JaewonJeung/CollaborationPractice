3401e55a3c7fe252cc393ac39dc1bc800a9c4b94
Nancy Quan <quan.nancy00@gmail.com>
2020-05-15 23:56:43 -0400

    nugget is hard to chewy

--- src/Practice.java
+++ src/Practice.java
@@ -2,5 +2,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("make sure you pull and resolve potential conflicts before you commit and push!");
         //nice
+        System.out.println("sleep is for the weak who knows what to do to be strong");
     }
 }
