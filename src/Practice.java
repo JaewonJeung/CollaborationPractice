24279d4f54080a31cc059bf847cd5b15920737a4
Nancy Quan <quan.nancy00@gmail.com>
2020-05-16 00:52:53 -0400

    hungry and sleepy

--- src/Practice.java
+++ src/Practice.java
@@ -3,5 +3,6 @@ public class Practice {
         System.out.println("make sure you pull good change here");
         //nice
         System.out.println("sleep is for the weak who knows what to do to be strong");
+        System.out.println("night. sleep well with some good melatonin stuff");
     }
 }
